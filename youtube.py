7512659603a763c78e0b76f0623297be99a4399f
andersonscorreia <andersonsantos.ifrn@gmail.com>
2022-08-16 11:30:40 -0300

    Api youtube pesquisa pronto

--- youtube.py
+++ youtube.py
@@ -2,12 +2,25 @@ import requests
 YOUR_API_KEY = ''
 SEARCH = 'ifrn'
 TYPE = 'video'
-RESULT_NUM = '3'
-
-
+RESULT_NUM = '10'
 
 
 r = requests.get(f'https://youtube.googleapis.com/youtube/v3/search?title&maxResults={RESULT_NUM}&q={SEARCH}%20&type={TYPE}&key={YOUR_API_KEY}')
 listVideo = r.json()
-
-print(listVideo)
+lista = listVideo['items']
+listId = [] 
+videos = '\n'
+for i in lista:
+    
+  a = i['id']['videoId']
+  listId.append(a) 
+for ID in listId:    
+    p = requests.get(f'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id={ID}&key={YOUR_API_KEY}')
+    descri = p.json()    
+   
+    for i in descri['items']:           
+        canal = i['snippet']['channelTitle']
+        titulo = i['snippet']['title']
+        link = f'https://www.youtube.com/watch?v={ID}'
+        videos += (f'Canal: {canal}\nTítulo: {titulo}\nLink: {link}\n\n') 
+print(videos)
