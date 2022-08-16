e99895a7e46bea13977ffdb6ea1d74032474747c
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2022-08-15 21:30:14 -0300

    lista de id

--- youtube.py
+++ youtube.py
@@ -5,9 +5,13 @@ TYPE = 'video'
 RESULT_NUM = '3'
 
 
-
-
 r = requests.get(f'https://youtube.googleapis.com/youtube/v3/search?title&maxResults={RESULT_NUM}&q={SEARCH}%20&type={TYPE}&key={YOUR_API_KEY}')
 listVideo = r.json()
+lista = listVideo['items']
+listId = [] 
+for i in lista:
+    
+  a = i['id']['videoId']
+  listId.append(a) 
 
-print(listVideo)
+print(listId) 
