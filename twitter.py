aec03570ed84f35d10e9591ab0f4d0a3a90a633a
Anderson Correia <41764860+andersonscorreia@users.noreply.github.com>
2022-08-22 19:24:03 -0300

    buscando dados do twitter

--- twitter.py
+++ twitter.py
@@ -1,6 +1,6 @@
 import requests
-YOUR_API_KEY = ''
-username= 'isaac_luis2012'
+
+username= 'elonmusk'
 TOKEN = ''
 headers = {
                'Authorization': f'Bearer {TOKEN}',
@@ -8,7 +8,7 @@ headers = {
            }
 
 
-r = requests.get(f'https://api.twitter.com/2/users/by/username/{username}?user.fields=name%2Cdescription%2Cpublic_metrics%2Cverified',headers = headers )
+r = requests.get(f'https://api.twitter.com/2/users/by/username/{username}?user.fields=name%2Cdescription%2Cpublic_metrics%2Cverified%2Clocation',headers = headers )
 a =r.json()
 print(a)
 
